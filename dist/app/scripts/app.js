cb4995248ca43e8b3082804116621c5806235fb2
harshilkumar <hrpatel.263>
2017-05-19 01:17:44 -0700

    Update the new files

---
+++ dist/app/scripts/app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by harshilkumar on 5/18/17.
+ */
+
+$(document).ready(function() {
+
+    $(document).on("click", "#menu_btn", function () {
+        console.log("on click menu btn");
+        $( ".collapsable_div" ).slideToggle( "slow" );
+    });
+});
