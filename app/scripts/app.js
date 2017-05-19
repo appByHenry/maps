a6e7cce9489b38069bb0f084fa9d896cb90e0b79
harshilkumar <hrpatel.263>
2017-05-19 01:22:26 -0700

    Update hte new files

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,6 +4,7 @@
 
 $(document).ready(function() {
 
+
     $(document).on("click", "#menu_btn", function () {
         console.log("on click menu btn");
         $( ".collapsable_div" ).slideToggle( "slow" );
